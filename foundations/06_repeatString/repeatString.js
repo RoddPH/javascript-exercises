57f5c97937cbb6cbd88a2c5efb88a508a18b915c
RoddPH <170872082+RoddPH@users.noreply.github.com>
2026-02-26 22:02:36 +0800

    Finished exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let hold = "";
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            hold += string;
+        }
+    } else if (num < 0) {
+        return "ERROR";
+    }
+    return hold;
 };
 
 // Do not edit below this line
